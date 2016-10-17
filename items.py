049ae204f097fad605d4b5f972c371139bb2598f
KwantomSolace <bryson_kwan@yahoo.co.uk>
2016-10-17 13:13:50 +0100

    List of items

---
+++ items.py
@@ -0,0 +1,57 @@
+item_photo = {
+    "id": "photo",
+
+    "name": "A photo of Bill Clinton, in a bar, with a girl",
+
+    "description":
+    """In a dark corner of the room you see Bill Clinton with
+his arm around a young attractive female, you know this is
+some useful dirt that could be used against Hillary, so you
+take a picture."""
+}
+
+item_satans_number = {
+    "id": "satans_number",
+
+    "name": "satans phone number",
+
+    "description":
+    """You go over to the bar and ask for a drink, Satan
+recognises you and says it’s on the house, you chat until
+you finish you drinking and he gives you his number, telling
+you to call him whenever you need his help"""
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "a million dollars",
+
+    "description":
+    """You grab a small pile of money from the floor, it totals
+to a million dollars"""
+}
+
+item_eagle = {
+    "id": "eagle",
+
+    "name": "eagle",
+
+    "description": "Option to steal eagle from cage."
+}
+
+item_portrait = {
+    "id": "portrait",
+    
+    "name": "a portrait of Donald Trump",
+
+    "description": "A picture of yourself, Mr Donald Trump"
+}
+
+item_hair = {
+    "id": "hair",
+    
+    "name": "A hair brush and glue",
+
+    "description": "absolutely no correlation between the two objects."
+}
