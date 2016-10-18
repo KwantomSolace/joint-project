47ebaf349f91ddaa41d291adf326ceda92c63b55
KwantomSolace <bryson_kwan@yahoo.co.uk>
2016-10-18 13:22:36 +0100

    the special attacks

---
+++ specialattacks.py
@@ -0,0 +1,9 @@
+spatk = [spatk_eagle, spatk_satan, spatk_photo, spatk_money]
+
+trump_spatk_eagle = "You open your suit jacket and release the bald eagle, it swoops around the debate room majestically and lands dramatically perching on your forearm and the crowd cheer in adoration.  You have gained 5 million more votes thanks to your prestigious showmanship"
+
+trump_spatk_satan = "You take out your phone and give Satan a call. A dark cloud covers the celling of the debate room and a hole appears from the ground, a few centimetres from where Hillary is standing. Her eyes glow a darkred colour, she starts to laugh and convulse uncontrollably. Satan appears to have possessed her and she is scaring all of the audience and viewers at home. She loses 6 million votes giving you 10 million more."
+
+trump_spatk_photo = "You take out the photograph of Bill Clinton with an attractive female. Hillary looks at it gritting her teeth, creating a pained smile, she looks angry and appears to now be a lot more distracted than before.  You gain 8 million votes"
+
+trump_spatk_money = "You make it rain! Money floods the stage from the ceiling, you look over to Hillary, she’s not impressed but the crowd go wild. You gain 3 million votes"
